c89560793086f692666673b555e3467a917c573e
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-05 12:22:00 +0600

    Create secondLargest.cpp

---
+++ secondLargest.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[5] = {1, 2, 4, 3, 5};
+    int largest = arr[0];
+    int secondLargest = arr[1];
+    
+    for (int i = n-2; i <= 0 ; i--) {
+        if (arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+
+    }
+    
+    cout << "largest: " << largest << endl;
+    cout << "secondLargest: " << secondLargest << endl;
+    return 0;
+}
