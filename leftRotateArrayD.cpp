86b300fd7baa02afe3e714dcac6b038bed4a368d
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-06 20:11:17 +0600

    brute solution added

--- leftRotateArrayD.cpp
+++ leftRotateArrayD.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 int main() {
+    // brute
+    temp[]= [1,2,3];
+    // shifting
+    for (int i = d; i < n; i++)
+    {
+        a[i-d] = a[i];
+    }
+    // put back temp
+    int j = 0;
+    for (int i = n-d; i < n; i++)
+    {
+        a[i] = temp[j];
+        j++
+    }
+    
+    
     cout << "" << endl;
     return 0;
 }
