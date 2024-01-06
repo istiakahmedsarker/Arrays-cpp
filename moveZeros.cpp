02d750d8c3e8fca30a5d443534ff8639b41b4997
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-06 20:39:35 +0600

    brute force added

--- moveZeros.cpp
+++ moveZeros.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 
 int main() {
+    arr[12] = {1,0,2,3,0,4,5,0,6,0,7,8};
+
+    // step 1
+    for (int i = 0; i < n; i++)
+    {
+        if(a[i] != 0){
+            temp.push_back(a[i]);
+        }
+    }
+    // step 2
+    int nz = temp.size();
+    for (int i = 0; i < nz; i++)
+    {
+        a[i] = temp[i];
+    }
+    for (int i = nz; i < n; i++)
+    {
+        a[i] = 0;
+    }
+    
+    
+    
     cout << "" << endl;
     return 0;
 }
