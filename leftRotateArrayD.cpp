8a150045b5ac4e08ae6495c2e1b7f3340eb21a5f
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-06 20:16:04 +0600

    optimal solution added

--- leftRotateArrayD.cpp
+++ leftRotateArrayD.cpp
@@ -1,23 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     // brute
-    temp[]= [1,2,3];
+    temp[] = [ 1, 2, 3 ];
     // shifting
     for (int i = d; i < n; i++)
     {
-        a[i-d] = a[i];
+        a[i - d] = a[i];
     }
     // put back temp
     int j = 0;
-    for (int i = n-d; i < n; i++)
+    for (int i = d; i < n; i++)
     {
         a[i] = temp[j];
         j++
     }
-    
-    
+
+    // ! optimal
+    for (int i = n - d; i < n; i++)
+    {
+        a[i] = temp[i - (n - d)];
+        j++
+    }
+    for (int i = 0; i < d; i++)
+    {
+        temp.pushback(arr[i]);
+    }
+
     cout << "" << endl;
     return 0;
 }
