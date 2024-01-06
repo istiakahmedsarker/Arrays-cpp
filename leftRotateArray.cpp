15e8fac974744929518e2f497770d99dc8bfef8e
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-06 11:36:06 +0600

    Update leftRotateArray.cpp

--- leftRotateArray.cpp
+++ leftRotateArray.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main() {
+    temp = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        arr[i-1] = arr[i];
+    }
+    arr[n-1] = temp;
+    
     cout << "" << endl;
     return 0;
 }
