9fdbed511d8343f90c0ed96b9ce46f3c4e548cc5
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-07 11:52:42 +0600

    optimal solution added

--- UnionOfArray.cpp
+++ UnionOfArray.cpp
@@ -3,25 +3,29 @@
 using namespace std;
 
 // ! brute force
-int main() {
+int main()
+{
     int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8};
     int arr2[] = {1, 2, 3, 4, 5, 6, 9, 10};
     int n = sizeof(arr1) / sizeof(arr1[0]);
 
     set<int> st;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         st.insert(arr1[i]);
         st.insert(arr2[i]);
     }
 
     int unionArr[2 * n];
     int i = 0;
-    for (auto it : st) {
+    for (auto it : st)
+    {
         unionArr[i++] = it;
     }
 
     cout << "Union: ";
-    for (int j = 0; j < i; j++) {
+    for (int j = 0; j < i; j++)
+    {
         cout << unionArr[j] << " ";
     }
     cout << endl;
@@ -35,7 +39,8 @@ int main() {
 #include <set>
 using namespace std;
 
-int main() {
+int main()
+{
     int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8};
     int arr2[] = {1, 2, 3, 4, 5, 6, 9, 10};
     int n1 = arr1.size();
@@ -44,10 +49,41 @@ int main() {
     int j = 0;
     vector<int> unionArr;
 
-    while(i<n1 && j<n2) {
-        if(arr1[i] <= arr2[j]) {
-            if()
+    while (i < n1 && j < n2)
+    {
+        if (arr1[i] <= arr2[j])
+        {
+            if (unionArr.size() == 0 || unionArr.back() != a[i])
+            {
+                unionArr.push_back(arr1[i]);
+            }
+            i++;
         }
+        else
+        {
+            if (unionArr.size() == 0 || unionArr.back() != b[j])
+            {
+                unionArr.push_back(arr1[i]);
+            }
+            j++;
+        }
+    }
+
+    while (j < n2)
+    {
+        if (unionArr.size() == 0 || unionArr.back() != b[j])
+        {
+            unionArr.push_back(arr1[i]);
+        }
+        j++;
+    }
+    while (i < n1)
+    {
+        if (unionArr.size() == 0 || unionArr.back() != a[i])
+            {
+                unionArr.push_back(arr1[i]);
+            }
+            i++;
     }
 
     return unionArr;
