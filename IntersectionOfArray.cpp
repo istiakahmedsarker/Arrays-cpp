8f14b21e3937d31d6e2c1a094ba2df40cbfe37f1
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-07 12:26:50 +0600

    brute force solution added

--- IntersectionOfArray.cpp
+++ IntersectionOfArray.cpp
@@ -2,6 +2,25 @@
 using namespace std;
 
 int main() {
+    // ! brute force
+    vector<int> ans;
+    int vis[m] = {0};
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (A[i] == B[j] && vis[j]==0){
+                ans.push_back(A[i]);
+                vis[j]= 1;
+                break;
+            }
+            if (B[j] > A[i]) break;
+            
+        }
+        
+    }
+    
     cout << "" << endl;
     return 0;
 }
