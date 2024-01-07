c83eacdd4ee4c9f1851417312379df4c9d1822b5
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-07 10:21:32 +0600

    solved the problem

--- LinerSearch.cpp
+++ LinerSearch.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main() {
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i] == num){
+            return i;
+        }
+    }
+        return -1;
+    
     cout << "" << endl;
     return 0;
 }
