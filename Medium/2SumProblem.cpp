0f1b1d77794d4e852233ca624e40a2af1f84229e
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-09 12:22:00 +0600

    better sol added

--- Medium/2SumProblem.cpp
+++ Medium/2SumProblem.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 // ! brute force
 int main() {
-    vector<int> arr = {2, 6, 5, 8, 11};
+    vector<int> arr = {2, 6, 5, 8, 11,3};
     int n = arr.size();
     int target = 14;
     int ans = 0;
@@ -18,5 +18,32 @@ int main() {
     }
     
     cout << ans << endl;
+    return 0;
+}
+
+// ! Better sol
+#include <bits/stdc++.h>
+using namespace std;
+
+string twoSum(int n, vector<int> &arr, int target) {
+    unordered_map<int, int> mpp;
+    for (int i = 0; i < n; i++) {
+        int num = arr[i];
+        int moreNeeded = target - num;
+        if (mpp.find(moreNeeded) != mpp.end()) {
+            return "YES";
+        }
+        mpp[num] = i;
+    }
+    return "NO";
+}
+
+int main()
+{
+    int n = 5;
+    vector<int> arr = {2, 6, 5, 8, 11};
+    int target = 14;
+    string ans = twoSum(n, arr, target);
+    cout << "This is the answer for variant 1: " << ans << endl;
     return 0;
 }
