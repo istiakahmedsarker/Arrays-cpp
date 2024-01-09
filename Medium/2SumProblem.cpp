fe19bc926d9914db036bc5c1c25856041804488a
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-09 12:12:49 +0600

    brute force added

--- Medium/2SumProblem.cpp
+++ Medium/2SumProblem.cpp
@@ -1,7 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// ! brute force
 int main() {
-    cout << "" << endl;
+    vector<int> arr = {2, 6, 5, 8, 11};
+    int n = arr.size();
+    int target = 14;
+    int ans = 0;
+
+    for (int i = 0; i < n; i++) {
+        int a = arr[i];
+        for (int j = i + 1; j < n; j++) {
+            if (a + arr[j] == target) {
+                ans = ans + 1;
+            }
+        }
+    }
+    
+    cout << ans << endl;
     return 0;
 }
